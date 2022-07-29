00d892dac6bbaf96b1f5283e6b514bb8509fbce4
olasrum <mursalo@gmail.com>
2022-07-29 18:05:47 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(array) {
+  const win = array.find(function(r) {return r.result === 'W'})
+  if (win) {
+    return win.year
+  } else {
+    return undefined
+  }
+};
+
+
+
+
 
